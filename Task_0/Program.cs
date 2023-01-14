1063e7fb41e6945ef878441b7ed6d8f25ea18928
«Olga <olis.9044@gmail.com>
2023-01-14 17:25:30 +0300

    Полностью дополнен файл с описанием задания

--- Task_0/Program.cs
+++ Task_0/Program.cs
@@ -2,9 +2,34 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
 
-Console.Write ("Введите количество элементов");
-int size = Convert.ToInt32 (Console.ReadLine () );
+Console.WriteLine("Введите количество элементов");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите строки");
 string [] arrayStrings = new string [size];
 string [] arrayFinal = new string [size];
 int len = 3;
 int pos = 0;
+
+void ArrayStrings ()
+{
+    for (int i = 0; i < size; i++)
+{
+    string element = Console.ReadLine(); 
+     arrayStrings [i] = element;
+}
+Console.Write($"[{string.Join(", " , arrayStrings)}] "); 
+for (int j = 0; j < size; j++)
+{
+    if (arrayStrings[j].Length <= len) 
+{
+        arrayFinal[pos] = arrayStrings[j]; 
+        pos++; 
+}
+}
+arrayFinal = arrayFinal.Where(x => x != null).ToArray();
+
+Console.Write($"[{string.Join(", " , arrayFinal)}] ");
+} 
+ArrayStrings();
+ 
+
